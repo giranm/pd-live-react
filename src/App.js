4916dd8d33fc014ab3f336c22185d6e0340751c9
Giran Moodley <giranmoo@gmail.com>
2021-06-03 22:11:43 +0100

    Implemented initial UI with mock data

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Navbar, Container } from 'react-bootstrap';
 
 import IncidentTableComponent from "./components/IncidentTable/IncidentTableComponent";
 
-import logo from "./assets/pd_logo.png";
+import logo from "./assets/images/pd_logo.png";
 import './App.css';
 
 function App() {
@@ -13,17 +13,13 @@ function App() {
           <Navbar.Brand>
             <img className="nav-bar-logo" src={logo} alt="logo" />
           </Navbar.Brand>
-          <Navbar.Brand>
+          <Navbar.Brand className="font-weight-bold">
             PagerDuty Live Incidents Console
-        </Navbar.Brand>
+          </Navbar.Brand>
         </Navbar>
       </div>
       <Container fluid>
-        <div className="config-ctr">
-          Configuration Settings
-        </div>
-        <div className="config-ctr">
-          Incidents Table
+        <div className="incidents-table-ctr">
           <IncidentTableComponent />
         </div>
       </Container>
