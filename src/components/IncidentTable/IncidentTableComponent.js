e7b9bf86c7bc9c0188f42eb40417a64fd62768df
Giran Moodley <giranmoo@gmail.com>
2021-06-17 18:43:38 +0100

    Updated no results modal

--- src/components/IncidentTable/IncidentTableComponent.js
+++ src/components/IncidentTable/IncidentTableComponent.js
@@ -3,8 +3,20 @@ import { connect } from "react-redux";
 import DataTable from "react-data-table-component";
 import DataTableExtensions from 'react-data-table-component-extensions';
 
+// import { ReactComponent as EmptyIncidents } from "assets/images/empty_incidents.svg"
+
 import 'react-data-table-component-extensions/dist/index.css';
 import "./IncidentTableComponent.css";
+
+
+const EmptyIncidentsComponent = () => {
+  return (
+    <div>
+      {/* <EmptyIncidents /> cannot import this for some reason */}
+      No incidents found!
+    </div>
+  )
+}
 
 const IncidentTableComponent = ({ incidents }) => {
   let columns = [
@@ -60,6 +72,7 @@ const IncidentTableComponent = ({ incidents }) => {
           fixedHeaderScrollHeight={"45vh"}
           pagination={true}
           paginationRowsPerPageOptions={[10, 15, 20, 25, 30]}
+          noDataComponent={EmptyIncidentsComponent()}
         />
       </DataTableExtensions>
     </div>
