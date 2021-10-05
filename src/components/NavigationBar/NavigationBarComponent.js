d0948c3c3de581a4b664c27461196a536742bfde
Giran Moodley <giranmoo@gmail.com>
2021-10-05 23:37:41 +0100

    Updated nav bar UI for product consistency

--- src/components/NavigationBar/NavigationBarComponent.js
+++ src/components/NavigationBar/NavigationBarComponent.js
@@ -1,9 +1,7 @@
 import { connect } from 'react-redux';
-
 import { Navbar, Nav } from 'react-bootstrap';
 
 import { ReactComponent as SettingsCog } from 'assets/images/settings_cog.svg';
-import logo from 'assets/images/pd_logo.png';
 
 import './NavigationBarComponent.css';
 
@@ -11,11 +9,15 @@ import { toggleDisplayQuerySettings } from 'redux/query_settings/actions';
 
 const NavigationBarComponent = ({ toggleDisplayQuerySettings }) => (
   <div className="navbar-ctr">
-    <Navbar bg="dark" variant="dark">
-      <Navbar.Brand>
-        <img className="nav-bar-logo" src={logo} alt="logo" />
+    <Navbar bg="light" variant="light">
+      <Navbar.Brand className="nav-bar-logo-ctr">
+        <div className="nav-bar-logo-black" href="/" alt="PagerDuty home page">
+          PagerDuty
+        </div>
       </Navbar.Brand>
-      <Navbar.Brand className="font-weight-bold">PagerDuty Live Incidents Console</Navbar.Brand>
+      <Navbar.Brand className="font-weight-bold">
+        Live Incidents Console
+      </Navbar.Brand>
       <Nav.Item className="ml-auto">
         <SettingsCog className="settings-toggle" onClick={() => toggleDisplayQuerySettings()} />
       </Nav.Item>
