3d74ed47e5d31e70d002be6e314e124445e03122
Giran Moodley <giranmoo@gmail.com>
2022-02-02 13:27:24 +0000

    Identified and fix issue with sessionStorage race condition - now clearing each test on startup.

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,20 +1,18 @@
 import {
-  acceptDisclaimer,
+  acceptDisclaimer, waitForIncidentTable,
 } from '../support/util/common';
 
 describe('PagerDuty Live', () => {
   beforeEach(() => {
     acceptDisclaimer();
+    waitForIncidentTable();
   });
 
   it('Renders the main application page', () => {
-    cy.visit('/');
     cy.get('.navbar-ctr').contains('Live Incidents Console');
   });
 
-  // FIXME: Both tests work in headful mode but fail in headless
   it('Application indicates when the required ability is available on the account', () => {
-    cy.visit('/');
     cy.get('.status-beacon-ctr').trigger('mouseover');
     cy.get('.status-beacon-connection').should('be.visible');
     cy.get('.status-beacon-connection').contains('Connected', { timeout: 15000 });
@@ -22,7 +20,6 @@ describe('PagerDuty Live', () => {
 
   it('Application indicates when the required ability is missing/disabled on the account', () => {
     // Intercept call to PagerDuty API with mock fixture
-    cy.visit('/');
     cy.intercept('https://api.pagerduty.com/abilities', {
       abilities: ['teams', 'read_only_users', 'service_support_hours', 'urgencies'],
     }).as('getAbilities');
