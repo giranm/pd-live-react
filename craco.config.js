5f64624f71b8c2b33c288871a32e155f541c8d20
Giran Moodley <giranmoo@gmail.com>
2022-05-07 21:04:54 +0100

    Revert react-scripts to last working version

--- craco.config.js
+++ craco.config.js
@@ -14,19 +14,4 @@ module.exports = {
       },
     ],
   },
-  // FIX: https://github.com/facebook/create-react-app/discussions/11767#discussioncomment-2421668
-  webpack: {
-    configure: {
-      ignoreWarnings: [
-        function ignoreSourcemapsloaderWarnings(warning) {
-          return (
-            warning.module
-            && warning.module.resource.includes('node_modules')
-            && warning.details
-            && warning.details.includes('source-map-loader')
-          );
-        },
-      ],
-    },
-  },
 };
