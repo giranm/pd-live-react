bca490525dfbabeabbd52f4d327b15a0cd511559
Giran Moodley <giranmoo@gmail.com>
2022-06-07 11:59:56 +0100

    Made ConfirmQueryModalComponent test more prescriptive

--- src/components/ConfirmQueryModal/ConfirmQueryModalComponent.test.js
+++ src/components/ConfirmQueryModal/ConfirmQueryModalComponent.test.js
@@ -6,19 +6,29 @@ import {
   MAX_INCIDENTS_LIMIT,
 } from 'config/constants';
 
+import {
+  generateRandomInteger,
+} from 'util/helpers';
+
 import ConfirmQueryModalComponent from './ConfirmQueryModalComponent';
 
 describe('ConfirmQueryModalComponent', () => {
   it('should render modal noting max incident limit has been reached', () => {
-    const totalIncidentsFromQuery = Math.floor(Math.random() * 200 + 200);
+    const totalIncidentsFromQuery = generateRandomInteger(
+      MAX_INCIDENTS_LIMIT + 1,
+      MAX_INCIDENTS_LIMIT * 2,
+    );
     const store = mockStore({
       querySettings: {
         displayConfirmQueryModal: true,
         totalIncidentsFromQuery,
       },
     });
+
     const wrapper = componentWrapper(store, ConfirmQueryModalComponent);
-    expect(wrapper.contains('Max Incidents Limit Reached')).toBeTruthy();
+    expect(wrapper.find('.modal-title').at(0).getDOMNode().textContent).toEqual(
+      'Max Incidents Limit Reached',
+    );
     expect(wrapper.find('.modal-body').at(0).getDOMNode().textContent).toEqual(
       [
         `Current query parameters match\u00A0${totalIncidentsFromQuery}\u00A0incidents.`,
