613b824979e748fe0ad0f16551b9500d35576bec
Giran Moodley <giranmoo@gmail.com>
2022-06-09 23:52:30 +0100

    Fixed integration tests for saga when mocking API - further test tbd.

--- src/redux/query_settings/sagas.test.js
+++ src/redux/query_settings/sagas.test.js
@@ -1,15 +1,23 @@
 import 'mocks/pdoauth';
 
 import {
-  select, call,
+  select,
 } from 'redux-saga/effects';
 import {
   expectSaga,
 } from 'redux-saga-test-plan';
+import * as matchers from 'redux-saga-test-plan/matchers';
+
+import {
+  MAX_INCIDENTS_LIMIT,
+} from 'config/constants';
 
 import {
   pd,
 } from 'util/pd-api-wrapper';
+import {
+  generateRandomInteger,
+} from 'util/helpers';
 
 import {
   generateMockIncidents,
@@ -29,71 +37,60 @@ import {
 import querySettings from './reducers';
 import selectQuerySettings from './selectors';
 import {
-  validateIncidentQuery, validateIncidentQueryImpl,
+  validateIncidentQueryImpl,
 } from './sagas';
 
 describe('Sagas: Query Settings', () => {
   const mockIncidents = generateMockIncidents(1);
-  const sinceDateObj = new Date();
-  const incidentStatus = ['triggered'];
-  const incidentUrgency = ['high'];
-  const teamIds = [];
-  const serviceIds = [];
-  const mockParams = {
-    since: sinceDateObj.toISOString(),
-    until: new Date().toISOString(),
-    limit: 1,
-    total: true,
+  const mockSelector = {
+    sinceDate: new Date(),
+    incidentStatus: ['triggered'],
+    incidentUrgency: ['high'],
+    teamIds: [],
+    serviceIds: [],
+  };
+  const expectedMockResponse = {
+    data: {
+      incidents: mockIncidents,
+      limit: 1,
+    },
   };
 
-  if (incidentStatus) mockParams['statuses[]'] = incidentStatus;
-  if (incidentUrgency) mockParams['urgencies[]'] = incidentUrgency;
-  if (teamIds.length) mockParams['team_ids[]'] = teamIds;
-  if (serviceIds.length) mockParams['service_ids[]'] = serviceIds;
+  it('validateIncidentQueryImpl: Within MAX_INCIDENTS_LIMIT', () => {
+    expectedMockResponse.data.total = generateRandomInteger(1, MAX_INCIDENTS_LIMIT);
+    expectedMockResponse.status = 200;
+    return expectSaga(validateIncidentQueryImpl)
+      .withReducer(querySettings)
+      .provide([
+        [select(selectQuerySettings), mockSelector],
+        [
+          // Matchers is used to mock API calls - ignores params used
+          matchers.call.fn(pd.get),
+          expectedMockResponse,
+        ],
+      ])
+      .silentRun()
+      .then((result) => {
+        // NB due to weird race condition, we can't accurately match on ISO Date string
+        expect(result.storeState.status).toEqual(CONFIRM_INCIDENT_QUERY_REQUESTED);
+      });
+  });
 
-  it('validateIncidentQuery', () => {
-    const mockResponse = {
-      data: {
-        incidents: mockIncidents,
-        limit: 1,
-        total: 500,
-      },
-      status: 200,
-    };
-
-    return (
-      expectSaga(validateIncidentQueryImpl)
-        .withReducer(querySettings)
-        .provide([
-          [
-            select(selectQuerySettings),
-            {
-              sinceDate: sinceDateObj,
-              incidentStatus,
-              incidentUrgency,
-              teamIds,
-              serviceIds,
-            },
-          ],
-          [call(pd.get, 'incidents', { data: { ...mockParams } }), mockResponse],
-        ])
-        // .call(pd.get, 'incidents', { data: { ...mockParams } })
-        .put({ type: VALIDATE_INCIDENT_QUERY_COMPLETED })
-        .put({
-          type: UPDATE_TOTAL_INCIDENTS_FROM_QUERY_REQUESTED,
-          totalIncidentsFromQuery: mockResponse.data.total,
-        })
-        .put({
-          type: CONFIRM_INCIDENT_QUERY_REQUESTED,
-          confirm: true,
-        })
-        .silentRun()
-        .then((result) => {
-          // NB due to weird race condition, we can't accurately match on ISO Date string
-          console.log(result.storeState);
-          // expect(result.storeState.status).toEqual(CONFIRM_INCIDENT_QUERY_REQUESTED);
-        })
-        .catch((err) => console.log(err))
+  it('validateIncidentQueryImpl: Over MAX_INCIDENTS_LIMIT', () => {
+    expectedMockResponse.data.total = generateRandomInteger(
+      MAX_INCIDENTS_LIMIT + 1,
+      MAX_INCIDENTS_LIMIT * 2,
     );
+    expectedMockResponse.status = 200;
+    return expectSaga(validateIncidentQueryImpl)
+      .withReducer(querySettings)
+      .provide([
+        [select(selectQuerySettings), mockSelector],
+        [matchers.call.fn(pd.get), expectedMockResponse],
+      ])
+      .silentRun()
+      .then((result) => {
+        expect(result.storeState.status).toEqual(TOGGLE_DISPLAY_CONFIRM_QUERY_MODAL_REQUESTED);
+      });
   });
 });
