2ec4e7a103220059d5ec31a61691259b6772a540
Giran Moodley <giranmoo@gmail.com>
2022-08-11 10:16:01 +0100

    Added replaceall shim for older node versions

--- setupTests.js
+++ setupTests.js
@@ -3,4 +3,8 @@ import {
 } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
+// eslint-disable-next-line import/no-extraneous-dependencies
+import replaceAllInserter from 'string.prototype.replaceall';
+
 configure({ adapter: new Adapter() });
+replaceAllInserter.shim();
