2d0d492c3faa758ee7350182a1e52589af03861b
Giran Moodley <giranmoo@gmail.com>
2022-10-10 22:51:54 +0100

    Upgraded bootstrap dependency and removed deprecated components.

--- src/components/GlobalSearch/GlobalSearchComponent.js
+++ src/components/GlobalSearch/GlobalSearchComponent.js
@@ -30,11 +30,9 @@ const GlobalSearchComponent = ({
       <Row>
         <Col>
           <InputGroup>
-            <InputGroup.Prepend>
-              <InputGroup.Text>
-                <SearchGlass />
-              </InputGroup.Text>
-            </InputGroup.Prepend>
+            <InputGroup.Text>
+              <SearchGlass />
+            </InputGroup.Text>
             <Form.Control
               id="global-search-input"
               placeholder="Search"
