1c03b8a6c1a8783704a9405076041b81e6767eba
Giran Moodley <giranmoo@gmail.com>
2023-01-18 22:04:47 +0000

    Added placeholders for incident details layout

--- src/components/IncidentDetailsModal/IncidentDetailsModalComponent.js
+++ src/components/IncidentDetailsModal/IncidentDetailsModalComponent.js
@@ -1,14 +1,24 @@
 import {
+  useState,
+} from 'react';
+import {
   connect,
 } from 'react-redux';
 
 import {
-  Modal,
+  Modal, Container, Row, Col, Tabs, Tab,
 } from 'react-bootstrap';
+
+import {
+  useTranslation,
+} from 'react-i18next';
 
 import {
   toggleDisplayIncidentDetailsModal as toggleDisplayIncidentDetailsModalConnected,
 } from 'redux/incident_details/actions';
+
+import AlertsTableComponent from './subcomponents/AlertsTableComponent';
+import IncidentTimelineComponent from './subcomponents/IncidentTimelineComponent';
 
 import './IncidentDetailsModalComponent.scss';
 
@@ -18,6 +28,11 @@ const IncidentDetailsModalComponent = ({
   const {
     displayIncidentDetailsModal,
   } = incidentDetails;
+  const {
+    t,
+  } = useTranslation();
+
+  const [currentTab, setCurrentTab] = useState('alerts');
 
   return (
     <div className="incident-details-modal-ctr">
@@ -30,7 +45,32 @@ const IncidentDetailsModalComponent = ({
         <Modal.Header closeButton>
           <Modal.Title>[#99999] Incident Summary Here</Modal.Title>
         </Modal.Header>
-        <Modal.Body className="modal-body-fullscreen">Incident Details Here</Modal.Body>
+        <Modal.Body className="modal-body-fullscreen">
+          <Container fluid>
+            <Row>
+              <Col>High Level Details Placeholder</Col>
+            </Row>
+            <Row>
+              <Col>Incident Actions Placeholder</Col>
+            </Row>
+            <Row>
+              <Col>Incident Fields Placeholder</Col>
+              <Col>Notes Placeholder</Col>
+            </Row>
+            <Row>
+              <Col>
+                <Tabs activeKey={currentTab} onSelect={(k) => setCurrentTab(k)}>
+                  <Tab eventKey="alerts" title={t('Alerts')}>
+                    <AlertsTableComponent />
+                  </Tab>
+                  <Tab eventKey="timeline" title={t('Timeline')}>
+                    <IncidentTimelineComponent />
+                  </Tab>
+                </Tabs>
+              </Col>
+            </Row>
+          </Container>
+        </Modal.Body>
       </Modal>
     </div>
   );
